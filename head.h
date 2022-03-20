bc17da95d881dc0bf08cd2bbcf83cf37e89898d5
MakVM <101976709+MakVM@users.noreply.github.com>
2022-03-20 15:29:43 +0300

    Update head.h

--- head.h
+++ head.h
@@ -2,5 +2,14 @@
 #define HEADER_FILE
 
 //the entire header file file
-
+#include<vector> // An exclusive feature of C++
+int factorial(int number)
+{
+  int iteration, factorial=1;
+  for(iteration=1; iteration<=number; iteration++)
+  {
+      factorial=factorial*iteration;
+  }
+  return factorial;
+}
 #endif
